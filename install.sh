d0259ac1a283d45504722bda437080cc845eedf9
Julien Desrosiers <jdesrosiers@audiokinetic.com>
2022-05-21 20:02:52 -0400

    xinitrc to display the time in dwm

--- install.sh
+++ install.sh
@@ -129,6 +129,7 @@ _install $PWD/gitconfig $HOME/.gitconfig
 _install $PWD/vimrc $HOME/.vimrc
 _install $PWD/ssh_rc.sh $HOME/.ssh/rc
 _install $PWD/xprofile $HOME/.xprofile
+_install $PWD/xinitrc $HOME/.xinitrc
 _install $PWD/inputrc $HOME/.inputrc
 _install_dir $PWD/vim $HOME/.vim
 _install_dir $PWD/screenlayout $HOME/.screenlayout
